a55d54230b3c4c002821e5ba9a63a12dc55746e4
Coding Masters-YT <codingsforlife@gmail.com>
2021-11-03 18:13:04 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+n = random
+password = []
+symbols = ['-' , '@']
+
+x = random.randint(0 , 22)
+y = random.choice(string.ascii_letters)
+d = random.choices(string.ascii_uppercase)
+z  = random.choices(symbols)
+c = z[0]
+e = d[0]
+for x in range(1):
+    print(x , c  ,y ,e , x , c  ,y ,e ,x , c  ,y ,e ,x , c  ,y ,e , x , c  ,y ,e ,x , c  ,y ,e,x , c  ,y ,e , x , c  ,y ,e ,x , c  ,y ,e ,x , c  ,y ,e , x , c  ,y ,e ,x , c  ,y ,e)
+  
+
+# password = [x , y]
+
+
+
+
